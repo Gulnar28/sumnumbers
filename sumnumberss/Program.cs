63fc9dc78ead0182fd6713dcdb6fee50a4e25dd8
gulnarvma@outlook.com <gulnarvma@outlook.com>
2022-12-11 12:25:41 +0400

    first commit

---
+++ sumnumberss/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace sumnumberss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+            
+            number1 = int.Parse(Console.ReadLine());
+            number2=int.Parse(Console.ReadLine());
+            int sum = number1 + number2;
+
+
+            Console.WriteLine(sum);
+        }
+    }
+}
